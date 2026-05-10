060e8ae8310a114275f319f1bc96c927cdc7fe89
Dmitry Petrov <dpetrov67@gmail.com>
2026-05-10 16:08:36 -0400

    fix: Replace [$address] with (array)$address in Horde_Mail_Rfc822::parseAddressList()
    This gracefully fixes trim(null) issue if $address is null.

--- lib/Horde/Mail/Rfc822.php
+++ lib/Horde/Mail/Rfc822.php
@@ -162,12 +162,8 @@ class Horde_Mail_Rfc822
             ? new Horde_Mail_Rfc822_List()
             : new Horde_Mail_Rfc822_GroupList();
 
-        if (!is_array($address)) {
-            $address = [$address];
-        }
-
         $tmp = [];
-        foreach ($address as $val) {
+        foreach ((array) $address as $val) {
             if ($val instanceof Horde_Mail_Rfc822_Object) {
                 $this->_listob->add($val);
             } else {
@@ -175,7 +171,7 @@ class Horde_Mail_Rfc822
             }
         }
 
-        if (!empty($tmp)) {
+        if ($tmp) {
             $this->_data = implode(',', $tmp);
             $this->_datalen = strlen($this->_data);
             $this->_ptr = 0;
